1527ffa2d93df796c33892d91c2a2e2a4b2419ce
abhishek84patel <mrabhishekpatel24@gmail.com>
2023-08-25 12:29:50 +0530

    arrow icon add

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const registerBtn = document.querySelector(".register");
 const LoginForm = document.querySelector(".Login");
 const RegisterForm = document.querySelector(".Register");
 const signUpBtn = document.querySelector(".signUp");
-// console.log(LoginForm, RegisterForm)
+const backBtn = document.querySelector(".back");
+
 loginBtn.addEventListener("click", () => {
 
     RegisterForm.classList.remove("register_show");
@@ -23,4 +24,10 @@ signUpBtn.addEventListener("click", () => {
     RegisterForm.classList.add("register_show");
     loginBtn.classList.remove("active")
     registerBtn.classList.add("active")
+})
+backBtn.addEventListener("click", () => {
+    RegisterForm.classList.remove("register_show");
+    LoginForm.classList.remove("Login-hide");
+    registerBtn.classList.remove("active")
+    loginBtn.classList.add("active")
 })
